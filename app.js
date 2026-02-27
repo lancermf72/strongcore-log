2b60be78d86f5a9dacb453ab2f2a4482c38f8ed0
lancermf72 <lancefink1972@gmail.com>
2026-02-27 02:13:50 +0100

    Refactor exercise addition in currentWorkout

--- app.js
+++ app.js
@@ -334,15 +334,19 @@ function generateWorkout() {
 
   const count = Math.min(5, shuffled.length);
 
+for (let i = 0; i < count; i++) {
+
   currentWorkout.exercises.push({
-  muscle: selectedMuscle,
-  name: shuffled[i].name,
-  draLevel: shuffled[i].draLevel || null,
-  hotelFriendly: shuffled[i].hotelFriendly || false,
-  sets: "",
-  reps: "",
-  weight: ""
-});
+    muscle: selectedMuscle,
+    name: shuffled[i].name,
+    draLevel: shuffled[i].draLevel || null,
+    hotelFriendly: shuffled[i].hotelFriendly || false,
+    sets: "",
+    reps: "",
+    weight: ""
+  });
+
+}
 
   alert("New filtered workout generated!");
   renderActiveWorkout();
